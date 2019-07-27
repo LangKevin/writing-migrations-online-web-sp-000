e020632b2c59322e395842aa6d9ab64119f11df1
kevin lang <langkevina@gmail.com>
2019-07-27 20:38:16 +0000

    Automatically backed up by Learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,13 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+  def up
+      change_table :students do |t|
+        t.change :price, :string
+      end
+    end
+   
+    def down
+      change_table :students do |t|
+        t.change :price, :integer
+      end
+    end
+end
